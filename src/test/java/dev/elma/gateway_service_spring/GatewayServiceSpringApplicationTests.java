805d462485143ca084e9bab8fbe26c890b5a823b
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-25 23:16:19 +0100

    Initial commit

---
+++ src/test/java/dev/elma/gateway_service_spring/GatewayServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.gateway_service_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
